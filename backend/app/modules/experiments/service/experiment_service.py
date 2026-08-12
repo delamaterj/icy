3b946d94cd137c4ed627b86d4877a10caddfc701
delamaterj <delamaterj@ufl.edu>
2026-08-12 11:33:23 -0400

    updated experiment validation / return for create experiment service

--- backend/app/modules/experiments/service/experiment_service.py
+++ backend/app/modules/experiments/service/experiment_service.py
@@ -4,6 +4,7 @@ from app.models.experiment import Experiment, ExperimentStatus
 from app.modules.datasets.repo.dataset_repo import DatasetRepository
 from app.modules.experiments.repo.experiment_repo import ExperimentRepository
 from app.modules.experiments.utils.experiment_validation import ExperimentValidation
+from app.modules.datasets.utils.dataset_parser import DatasetParser
 
 from app.common.exceptions import (
     ValidationException,
@@ -23,7 +24,7 @@ class ExperimentService:
         try:
 
             errors = ExperimentValidation.validate_create(data)
-            if errors:
+            if errors["errors"]:
                 raise ValidationException(errors)
 
             dataset = self.dataset_repository.get_by_id(
@@ -34,16 +35,26 @@ class ExperimentService:
                     "Dataset not found."
                 )
 
+            metadata = DatasetParser.extract_metadata( dataset.file_path ) 
+            columns = metadata["columns"] 
+            target_column = data["target_column"] 
+            if target_column not in columns: 
+                raise ValidationException([ f"Target column '{target_column}' " "does not exist in the selected dataset." ])
+
             if dataset.status != DatasetStatus.READY:
                 raise ValidationException(
                     ["Dataset must be READY before creating an experiment."]
                 )
 
-            experiment = Experiment(
-                dataset_id=dataset.id,
-                name=data["name"],
-                description=data.get("description"),
-                status=ExperimentStatus.CREATED,
+            experiment = Experiment( 
+                dataset_id=dataset.id, 
+                name=data["name"], 
+                description=data.get("description"), 
+                status=ExperimentStatus.CREATED, 
+                model=data["model"], 
+                target_column=data["target_column"], 
+                test_size=data.get("test_size"), 
+                random_seed=data.get("random_seed") 
             )
 
             experiment = self.experiment_repository.create(experiment)
