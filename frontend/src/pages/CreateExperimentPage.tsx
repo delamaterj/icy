48ab37a08b06449bf27b8152048af05125da5715
delamaterj <delamaterj@ufl.edu>
2026-08-14 11:47:25 -0400

    improved UI/UX for creating successful experiment

--- frontend/src/pages/CreateExperimentPage.tsx
+++ frontend/src/pages/CreateExperimentPage.tsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import ExperimentForm from "../components/experiments/ExperimentForm";
 import { useCreateExperiment } from "../hooks/useCreateExperiment";
 import type { CreateExperimentRequest } from "../types/experiment";
+import {useNavigate} from 'react-router-dom';
 
 export default function CreateExperimentPage() {
 
     const { dataset_id } = useParams();
+    const navigate = useNavigate();
 
     const {
         createExperiment,
@@ -18,6 +20,8 @@ export default function CreateExperimentPage() {
         try {
             const response = await createExperiment(data);
             console.log(response);
+            alert("Experiment successfully created");
+            navigate(`/experiments/${response.id}`, { replace: true } );
         }
         catch (err) {
             console.log(err);
