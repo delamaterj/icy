6b670f7c253cc4bd787e9325e7604c84bc0f57b2
delamaterj <delamaterj@ufl.edu>
2026-08-18 14:40:30 -0400

    updated imports for env.py and models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.dataset import Dataset
+from app.models.experiment import Experiment
+from app.models.training_run import TrainingRun
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,12 +1,10 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
-
 from app import create_app
 from app.extensions import db
+import app.models
 
 app = create_app()
 
