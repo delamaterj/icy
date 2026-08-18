6724d01c0e8eb37ca8d983399ba3ea719df84c47
delamaterj <delamaterj@ufl.edu>
2026-08-18 15:05:35 -0400

    added service for create training run

---
+++ backend/app/modules/training_runs/services/training_run_services.py
@@ -0,0 +1,50 @@
+from app.common.exceptions import ValidationException
+from app.enums.dataset_status import DatasetStatus
+from app.modules.experiments.repo.experiment_repo import (
+    ExperimentRepository
+)
+from app.modules.training_runs.repo.training_run_repo import (
+    TrainingRunRepository
+)
+
+class TrainingRunService:
+
+    def __init__(self):
+
+        self.experiment_repository = ExperimentRepository()
+        self.training_run_repository = TrainingRunRepository()
+
+
+    def create_training_run(self, experiment_id):
+
+        experiment = self.experiment_repository.get_by_id(
+            experiment_id
+        )
+
+        if not experiment:
+            raise ValidationException([
+                "Experiment not found."
+            ])
+
+        dataset = experiment.dataset
+
+        if not dataset:
+            raise ValidationException([
+                "Experiment dataset not found."
+            ])
+
+        if dataset.status != DatasetStatus.READY:
+            raise ValidationException([
+                "Experiment dataset must be READY."
+            ])
+
+        training_run = self.training_run_repository.create(
+            experiment.id
+        )
+
+        return {
+            "id": str(training_run.id),
+            "experiment_id": str(training_run.experiment_id),
+            "status": training_run.status.value,
+            "created_at": training_run.created_at.isoformat()
+        }
