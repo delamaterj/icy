5cf3070e447998b19eee1420da5ce39674374447
delamaterj <delamaterj@ufl.edu>
2026-08-21 12:28:35 -0400

    performed alembic migration for new results table

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.dataset import Dataset
 from app.models.experiment import Experiment
 from app.models.training_run import TrainingRun
+from app.models.training_run_results import TrainingRunResult
