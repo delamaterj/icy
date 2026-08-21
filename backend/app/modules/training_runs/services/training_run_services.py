8a5172fade2593ecffc37b208fe84214bcdc2648
delamaterj <delamaterj@ufl.edu>
2026-08-21 12:49:13 -0400

    added get and get:id services for tr

--- backend/app/modules/training_runs/services/training_run_services.py
+++ backend/app/modules/training_runs/services/training_run_services.py
@@ -48,3 +48,24 @@ class TrainingRunService:
             "status": training_run.status.value,
             "created_at": training_run.created_at.isoformat()
         }
+
+    def get_runs_by_experiment(experiment_id):
+
+        runs = TrainingRunRepository.get_by_experiment_id(
+            experiment_id
+        )
+
+        return runs
+
+    def get_run(run_id):
+
+        run = TrainingRunRepository.get_by_id(
+            run_id
+        )
+
+        if not run:
+            raise ValidationException([
+                "Training run not found."
+            ])
+
+        return run
