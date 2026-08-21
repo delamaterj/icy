0319b45349c22e8708cadcfac4f92dffd4e1db72
delamaterj <delamaterj@ufl.edu>
2026-08-21 12:38:58 -0400

    added repo for results

---
+++ backend/app/modules/training_run_results/repo/training_run_results_repo.py
@@ -0,0 +1,28 @@
+from app.extensions import db
+from app.models.training_run_results import TrainingRunResult
+
+class TrainingRunResultRepository:
+
+    @staticmethod
+    def create_result(
+        training_run_id,
+        accuracy,
+        precision,
+        recall,
+        f1_score,
+        confusion_matrix
+    ):
+
+        result = TrainingRunResult(
+            training_run_id=training_run_id,
+            accuracy=accuracy,
+            precision=precision,
+            recall=recall,
+            f1_score=f1_score,
+            confusion_matrix=confusion_matrix
+        )
+
+        db.session.add(result)
+        db.session.commit()
+
+        return result
