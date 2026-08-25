bea86bd870c21147be5123a7abc160be6efe0a0f
delamaterj <delamaterj@ufl.edu>
2026-08-25 12:31:58 -0400

    added controller for new run training run service

--- backend/app/modules/training_runs/controllers/training_run_controller.py
+++ backend/app/modules/training_runs/controllers/training_run_controller.py
@@ -70,10 +70,7 @@ def get_training_runs_controller(experiment_id):
         for run in runs
     ]), 200
 
-def get_training_run_controller(
-    experiment_id,
-    run_id
-):
+def get_training_run_controller(experiment_id, run_id):
 
     run = training_run_service.get_run(
         experiment_id,
@@ -83,3 +80,7 @@ def get_training_run_controller(
     return jsonify(
         serialize_training_run_details(run)
     ), 200
+
+def run_training_run_controller(run_id):
+    result = training_run_service.run_training_run(run_id)
+    return jsonify(result), 201
