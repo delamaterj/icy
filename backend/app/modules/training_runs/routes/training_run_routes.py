4bb56ec195b96f6887df01f34889e564965c9127
delamaterj <delamaterj@ufl.edu>
2026-08-25 12:32:20 -0400

    added route for new run training run service

--- backend/app/modules/training_runs/routes/training_run_routes.py
+++ backend/app/modules/training_runs/routes/training_run_routes.py
@@ -3,7 +3,8 @@ from flask import Blueprint
 from app.modules.training_runs.controllers.training_run_controller import (
     create_training_run_controller,
     get_training_runs_controller,
-    get_training_run_controller)
+    get_training_run_controller,
+    run_training_run_controller)
 
 training_run_bp = Blueprint("training_run", __name__)
 
@@ -18,3 +19,7 @@ def get_training_runs(experiment_id):
 @training_run_bp.get("/experiments/<uuid:experiment_id>/runs/<uuid:training_run_id>")
 def get_training_run(experiment_id, training_run_id):
     return get_training_run_controller(experiment_id, training_run_id)
+
+@training_run_bp.post("/training-runs/<uuid:training_run_id>/run")
+def run_training_run(training_run_id):
+    return run_training_run_controller(training_run_id)
