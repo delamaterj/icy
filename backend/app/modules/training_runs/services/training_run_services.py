73da3341cb25f47a2f349ea7431b37907fda79b5
delamaterj <delamaterj@ufl.edu>
2026-08-25 12:31:40 -0400

    added run training run service, migrated ML code into service

--- backend/app/modules/training_runs/services/training_run_services.py
+++ backend/app/modules/training_runs/services/training_run_services.py
@@ -53,75 +53,6 @@ class TrainingRunService:
             "status": training_run.status.value,
             "created_at": training_run.created_at.isoformat()
         }
-        #ML code to populate results
-        '''
-        try:
-            self.training_run_repository.update_status(
-                training_run.id,
-                TrainingRunStatus.RUNNING
-            )
-
-            data = DatasetLoader.load_and_split(
-                file_path=dataset.file_path,
-                target_column=experiment.target_column,
-                test_size=experiment.test_size,
-                random_seed=experiment.random_seed
-            )
-
-            model = ModelFactory.create(
-                experiment.model,
-                experiment.random_seed
-            )
-
-            trained_model = ModelTrainer.train(
-                model,
-                data["X_train"],
-                data["y_train"]
-            )
-
-            results = ModelEvaluator.evaluate(
-                trained_model,
-                data["X_test"],
-                data["y_test"]
-            )
-
-            
-            self.training_run_result_repository.create_result(
-                training_run_id=training_run.id,
-                accuracy=results["accuracy"],
-                precision=results["precision"],
-                recall=results["recall"],
-                f1_score=results["f1_score"],
-                confusion_matrix=results["confusion_matrix"]
-            )
-
-            self.training_run_repository.update_status(
-                training_run.id,
-                TrainingRunStatus.COMPLETED
-            )
-
-            return {
-                "id": str(training_run.id),
-                "experiment_id": str(training_run.experiment_id),
-                "status": training_run.status.value,
-                "created_at": training_run.created_at.isoformat()
-            }
-        except Exception as e:
-
-            self.training_run_repository.update_status(
-                training_run.id,
-                TrainingRunStatus.FAILED
-            )
-
-            self.training_run_repository.update_error_message(
-                training_run.id,
-                str(e)
-            )
-        '''
-
-        '''raise ValidationException([
-            "Training Run Failed."
-        ])'''
         
     def get_runs_by_experiment(self, experiment_id):
 
@@ -144,3 +75,99 @@ class TrainingRunService:
             raise ValidationException("Training run not found.")
 
         return run
+
+    def run_training_run(self, training_run_id):
+
+        training_run = self.training_run_repository.get_by_id(
+            training_run_id
+        )
+
+        if not training_run:
+            raise ValidationException([
+                "Training run not found."
+            ])
+
+        experiment = self.experiment_repository.get_by_id(
+            training_run.experiment_id
+        )
+        
+        if not experiment:
+            raise ValidationException([
+                "Experiment not found."
+            ])
+
+        dataset = experiment.dataset
+        
+        if not dataset:
+            raise ValidationException([
+                "Experiment dataset not found."
+            ])
+        
+        if training_run.status.value != "CREATED":
+            raise ValidationException([
+                "Training run must be created"
+            ])
+
+        try:
+            self.training_run_repository.update_status(
+                training_run.id,
+                TrainingRunStatus.RUNNING
+            )
+        
+            data = DatasetLoader.load_and_split(
+                file_path=dataset.file_path,
+                target_column=experiment.target_column,
+                test_size=experiment.test_size,
+                random_seed=experiment.random_seed
+            )
+        
+            model = ModelFactory.create(
+                experiment.model,
+                experiment.random_seed
+            )
+        
+            trained_model = ModelTrainer.train(
+                model,
+                data["X_train"],
+                data["y_train"]
+            )
+        
+            results = ModelEvaluator.evaluate(
+                trained_model,
+                data["X_test"],
+                data["y_test"]
+            )
+        
+                    
+            self.training_run_result_repository.create_result(
+                training_run_id=training_run.id,
+                accuracy=results["accuracy"],
+                precision=results["precision"],
+                recall=results["recall"],
+                f1_score=results["f1_score"],
+                confusion_matrix=results["confusion_matrix"]
+            )
+        
+            self.training_run_repository.update_status(
+                training_run.id,
+                TrainingRunStatus.COMPLETED
+            )
+        
+            return {
+                "id": str(training_run.id),
+                "experiment_id": str(training_run.experiment_id),
+                "status": training_run.status.value,
+                "created_at": training_run.created_at.isoformat()
+            }
+        
+        except Exception as e:
+        
+            self.training_run_repository.update_status(
+                training_run.id,
+                TrainingRunStatus.FAILED
+            )
+        
+            self.training_run_repository.update_error_message(
+                training_run.id,
+                str(e)
+            )
