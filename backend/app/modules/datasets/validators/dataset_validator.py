1d2f8328776ddb190e175a16195287f6081f4c41
delamaterj <delamaterj@ufl.edu>
2026-08-27 14:33:47 -0400

    updating dataset_validator imports

--- backend/app/modules/datasets/validators/dataset_validator.py
+++ backend/app/modules/datasets/validators/dataset_validator.py
@@ -1,5 +1,4 @@
 import os
-from app.enums.file_type import FileType
 from app.modules.datasets.validators.validation_result import ValidationResult
 
 
@@ -16,17 +15,6 @@ class DatasetValidator:
     ):
         if not os.path.exists(file_path):
             result.add_error("Dataset file does not exist")
-
-    def _validate_supported_file(
-        self,
-        file_type: FileType,
-        result: ValidationResult
-    ):
-        supported = {
-            FileType.CSV
-        }
-        if file_type not in supported:
-            result.add_error(f"Dataset type {file_type.value} is currently not supported for training")
 
     def _validate_not_empty(
         self,
@@ -64,7 +52,6 @@ class DatasetValidator:
         result = ValidationResult()
 
         self._validate_file_exists(dataset.file_path, result)
-        self._validate_supported_file(dataset.file_type, result)
         self._validate_not_empty(metadata, result)
         self._validate_required_columns(metadata, result)
 
