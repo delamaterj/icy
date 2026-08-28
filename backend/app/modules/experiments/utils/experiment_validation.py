d73388bae4551a88bd967638c89485248933ebd2
delamaterj <delamaterj@ufl.edu>
2026-08-28 12:51:29 -0400

    removed validation for experiment columns that do not exist anymore

--- backend/app/modules/experiments/utils/experiment_validation.py
+++ backend/app/modules/experiments/utils/experiment_validation.py
@@ -1,5 +1,6 @@
 import uuid 
 from app.enums.experiment_model import ExperimentModel 
+
 class ExperimentValidation:
 
     @staticmethod
@@ -40,17 +41,7 @@ class ExperimentValidation:
             errors.append("target_column is required.")
         elif len(target_column) > 255:
             errors.append("target_column must not exceed 255 characters.")
-        '''
-        test_size = data.get("test_size", 0.20)
-        if not isinstance(test_size, (int, float)) or isinstance(test_size, bool):
-            errors.append("test_size must be a number.")
-        elif not 0 < test_size < 1:
-            errors.append("test_size must be greater than 0 and less than 1.")
-        
-        random_seed = data.get("random_seed", 42)
-        if not isinstance(random_seed, int) or isinstance(random_seed, bool):
-            errors.append("random_seed must be an integer.")
-        '''
+            
         return {
             "errors": errors
         }
