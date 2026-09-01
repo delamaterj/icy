5c499c13e46e6a2b52b2242ee493c051e8608a88
delamaterj <delamaterj@ufl.edu>
2026-08-31 21:47:31 -0400

    added pages for trs/results

---
+++ frontend/src/pages/TrainingRunsPage.tsx
@@ -0,0 +1,31 @@
+import TrainingRunsTable from "../components/training_runs/TrainingRunsTable";
+import { useTrainingRuns } from "../hooks/useTrainingRuns";
+import { useParams } from "react-router-dom";
+
+export default function TrainingRunsPage() {
+
+    const { id } = useParams();
+
+    const {
+        trainingRuns,
+        loading,
+        error
+    } = useTrainingRuns(id ?? "");
+
+    if (loading) {
+        return <p>Loading training runs...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return (
+        <>
+            <h1>Training Runs</h1>
+            <TrainingRunsTable
+                training_runs={trainingRuns}
+            />
+        </>
+    );
+}
